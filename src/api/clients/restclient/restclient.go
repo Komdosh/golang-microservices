d6685a8dd526f4cc43bc024d274881869a8c69c8
Andrey Tabakov <komdosh@yandex.ru>
2020-11-04 12:45:58 +0300

    feat(testing): mockup rest client

--- src/api/clients/restclient/restclient.go
+++ src/api/clients/restclient/restclient.go
@@ -3,10 +3,52 @@ package restclient
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
 )
 
+var (
+	enabledMocks = false
+	mocks        = make(map[string]*Mock)
+)
+
+type Mock struct {
+	Url        string
+	HttpMethod string
+	Response   *http.Response
+	Err        error
+}
+
+func GetMockId(httpMethod string, url string) string {
+	return fmt.Sprintf("%s_%s", httpMethod, url)
+}
+
+func StartMockups() {
+	enabledMocks = true
+}
+
+func StopMockups() {
+	enabledMocks = false
+}
+
+func AddMockup(mock Mock) {
+	mocks[GetMockId(mock.HttpMethod, mock.Url)] = &mock
+}
+
+func FlushMockups() {
+	mocks = make(map[string]*Mock)
+}
+
 func Post(url string, body interface{}, headers http.Header) (*http.Response, error) {
+	if enabledMocks {
+		mock := mocks[GetMockId(http.MethodPost, url)]
+		if mock == nil {
+			return nil, errors.New(fmt.Sprintf("No mockup found for %s", url))
+		}
+		return mock.Response, mock.Err
+	}
+
 	jsonBytes, err := json.Marshal(body)
 	if err != nil {
 		return nil, err
--- src/api/providers/github_providers/github_provider_test.go
+++ src/api/providers/github_providers/github_provider_test.go
@@ -1,10 +1,27 @@
 package github_providers
 
 import (
-	"fmt"
+	"errors"
+	"github.com/Komdosh/golang-microservices/src/api/clients/restclient"
+	"github.com/Komdosh/golang-microservices/src/api/domain/github"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
 	"testing"
 )
+
+func TestMain(m *testing.M) {
+	restclient.StartMockups()
+	os.Exit(m.Run())
+}
+
+func TestConstants(t *testing.T) {
+	assert.EqualValues(t, "Authorization", headerAuthorization)
+	assert.EqualValues(t, "token %s", headerAuthorizationFormat)
+	assert.EqualValues(t, "https://api.github.com/user/repos", urlCreateRepo)
+}
 
 func TestGetAuthorizationHeader(t *testing.T) {
 	header := getAuthorizationHeader("abc123")
@@ -12,10 +29,120 @@ func TestGetAuthorizationHeader(t *testing.T) {
 	assert.EqualValues(t, "token abc123", header)
 }
 
-func TestDefer(t *testing.T) {
-	defer fmt.Println("1")
-	defer fmt.Println("2")
-	defer fmt.Println("3")
+func TestCreateRepoErrorRestClient(t *testing.T) {
+	restclient.FlushMockups()
+	restclient.AddMockup(restclient.Mock{
+		Url:        "https://api.github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Err:        errors.New("invalid RestClient response"),
+	})
 
-	fmt.Println("function's body")
+	response, err := CreateRepo("", github.CreateRepoRequest{})
+	assert.Nil(t, response)
+	assert.NotNil(t, err)
+
+	assert.EqualValues(t, "invalid RestClient response", err.Message)
+}
+
+func TestCreateRepoInvalidResponseBody(t *testing.T) {
+	restclient.FlushMockups()
+
+	invalidCloser, _ := os.Open("-asf3")
+	restclient.AddMockup(restclient.Mock{
+		Url:        "https://api.github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusCreated,
+			Body:       invalidCloser,
+		},
+	})
+
+	response, err := CreateRepo("", github.CreateRepoRequest{})
+	assert.Nil(t, response)
+	assert.NotNil(t, err)
+
+	assert.EqualValues(t, http.StatusInternalServerError, err.StatusCode)
+	assert.EqualValues(t, "invalid response body", err.Message)
+}
+
+func TestCreateRepoInvalidErrorInterface(t *testing.T) {
+	restclient.FlushMockups()
+
+	restclient.AddMockup(restclient.Mock{
+		Url:        "https://api.github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusUnauthorized,
+			Body:       ioutil.NopCloser(strings.NewReader(`{"message": 1}`)),
+		},
+	})
+
+	response, err := CreateRepo("", github.CreateRepoRequest{})
+	assert.Nil(t, response)
+	assert.NotNil(t, err)
+
+	assert.EqualValues(t, http.StatusInternalServerError, err.StatusCode)
+	assert.EqualValues(t, "invalid json response body", err.Message)
+}
+
+func TestCreateRepoUnauthorized(t *testing.T) {
+	restclient.FlushMockups()
+
+	restclient.AddMockup(restclient.Mock{
+		Url:        "https://api.github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusUnauthorized,
+			Body:       ioutil.NopCloser(strings.NewReader(`{"message": "Requires authentication", "documentation_url": "https://docs.github.com/rest/reference/repos#create-a-repository-for-the-authenticated-user"}`)),
+		},
+	})
+
+	response, err := CreateRepo("", github.CreateRepoRequest{})
+
+	assert.Nil(t, response)
+	assert.NotNil(t, err)
+
+	assert.EqualValues(t, http.StatusUnauthorized, err.StatusCode)
+	assert.EqualValues(t, "Requires authentication", err.Message)
+}
+
+func TestCreateRepoInvalidSuccessResponse(t *testing.T) {
+	restclient.FlushMockups()
+
+	restclient.AddMockup(restclient.Mock{
+		Url:        "https://api.github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusCreated,
+			Body:       ioutil.NopCloser(strings.NewReader(`{"id": "123"}`)),
+		},
+	})
+
+	response, err := CreateRepo("", github.CreateRepoRequest{})
+
+	assert.Nil(t, response)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.StatusCode)
+	assert.EqualValues(t, "error when trying to unmarshal github create repo response", err.Message)
+}
+
+func TestCreateRepoNoError(t *testing.T) {
+	restclient.FlushMockups()
+
+	restclient.AddMockup(restclient.Mock{
+		Url:        "https://api.github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusCreated,
+			Body:       ioutil.NopCloser(strings.NewReader(`{"id": 123,"name": "golang-awesome","full_name": "Komdosh/golang-awesome"}`)),
+		},
+	})
+
+	response, err := CreateRepo("", github.CreateRepoRequest{})
+
+	assert.Nil(t, err)
+	assert.NotNil(t, response)
+	assert.EqualValues(t, 123, response.Id)
+	assert.EqualValues(t, "golang-awesome", response.Name)
+	assert.EqualValues(t, "Komdosh/golang-awesome", response.FullName)
 }
